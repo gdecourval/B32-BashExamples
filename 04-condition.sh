aef57eeb172f8bec0a886e4a42e86c360acd8635
Gabriel de Courval-Pare <gdecourval@gmail.com>
2018-12-03 10:40:24 -0500

    Done with conditions

--- 04-condition.sh
+++ 04-condition.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
+note=60
 
+if test $note -lt 60
+then
+	echo "Désolé, vous devez reprendre le cours..."
+elif [[ $note -eq 60 ]]
+then
+	echo "Vous êtes à la limite!!"
+else
+	echo "Bravo!"
+fi
 
+read lettre
 
+case $lettre in
+	[[:lower:]])
+		echo "minuscule"
+		;;
+	*)
+		echo "autre..."
+		;;
+esac
 
-
-
-
+if test $lettre != "a"
+then
+	echo "la lettre n'est pas a"
+fi
