6f1df4f87e4968b60c67d70fc2ab0b1b771bd5d2
Gabriel de Courval-Pare <gdecourval@gmail.com>
2018-12-03 12:27:02 -0500

    Done with exercise 2

--- exercice2.sh
+++ exercice2.sh
@@ -1,1 +1,27 @@
 #!/bin/bash
+
+Menu()
+{
+	echo "======================="
+	echo "CVM SOFT"
+	echo "d = dictionnaire"
+	echo "q = quitter"
+}
+
+choix=a
+
+while [[ $choix != "q" ]]
+do
+	Menu
+	read -p "Votre choix: " choix
+	if [[ $choix = "d" ]];then
+		./exercice1.sh
+		sleep 1
+	fi
+done
+
+clear
+
+
+
+
--- exercice3.sh
+++ exercice3.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+for ((i=0;i<20;i++)
+do
+	clear
+	echo "@"
+
