9f3450e728b34d2f6107d9477e8f73c62663ceb5
Gabriel de Courval-Pare <gdecourval@gmail.com>
2018-12-03 10:51:33 -0500

    Done with loops

--- 05-boucles.sh
+++ 05-boucles.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
 
+for ((i=0;i<5;i++))
+do
+	echo $i
+done
+
+liste=`ls`
+
+# foreach
+for fic in $liste
+do
+	echo $fic
+done
+
+resultat=o
+
+#-o = or
+#-a = and
+
+while test $resultat = o -o $resultat = O
+do
+	read -p "Entrez o pour répéter la boucle : " resultat
+done
