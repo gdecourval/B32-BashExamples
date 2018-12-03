100fd2b2730a54edcf7021ed15efe1a19a0bfaae
Gabriel de Courval-Pare <gdecourval@gmail.com>
2018-12-03 09:49:41 -0500

    Done with hello world

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+# Ceci est un commentaire
+echo "Salut"
+
+# -n = empêche retour de ligne
+echo -n "Vive "
+echo "Linux"
+
+# -e Permet l'utilisation des caractères spéciaux
+echo -e "123\n123\n123"
+
+read -p "Vous avez quel âge?" age
+echo $age "ans! Vous paraissez plus jeune"
