b18675f6d3c42058700b0d1b3867e3bee2e2ca0f
Gabriel de Courval-Pare <gdecourval@gmail.com>
2018-12-03 10:02:17 -0500

    Done with variables

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 
+prenom=KimCharles
+echo $prenom
+
+# Forcer une déclaration d'une variable "int"
+declare -i age=33
+
+# Mettre une variable READ ONLY
+declare -r age2=33
+
+# Les variables systèmes
+echo $0
+echo $1
+echo $2
+
+echo $#
